02a154856e65b37b1528d6022632d103c35e763a
Wojciech Czarnocki <30754368+caren1@users.noreply.github.com>
2019-07-17 16:59:35 +0200

    Fixed the bodyparser

--- app.js
+++ app.js
@@ -7,10 +7,12 @@ const app = express();
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 
-app.get("/home", function(req, res){
+app.get("/", function(req, res){
   res.render("home");
 });
 
