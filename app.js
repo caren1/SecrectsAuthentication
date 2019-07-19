5557286e7a0a5b621c12e319c201009d3b56a417
Wojciech Czarnocki <30754368+caren1@users.noreply.github.com>
2019-07-19 19:20:33 +0200

    Finished the project, allowing users to add secrets - with usage of form - showing all the posted secrets.

--- app.js
+++ app.js
@@ -53,18 +53,50 @@ app.get("/logout", function(req, res) {
   res.redirect("/");
 });
 
-
-app.get("/secrets", function(req, res) {
-  if (req.isAuthenticated()) {
-    res.render("secrets");
-  } else {
+app.get("/submit", function(req, res){
+  if(req.isAuthenticated){
+    res.render("submit");
+  }else{
     res.redirect("/login");
   }
 });
 
+app.post("/submit", function(req, res){
+  const submittedSecret = req.body.secret;
+
+  User.findById(req.user.id, function(err, foundUser){
+    if(err){
+      console.log(err);
+    }else{
+      if(foundUser){
+        foundUser.secret = submittedSecret;
+        foundUser.save(function(){
+          res.redirect("/secrets");
+        });
+      }
+    }
+  });
+
+
+});
+
+
+app.get("/secrets", function(req, res) {
+  User.find({"secret" : {$ne: null}}, function(err, foundUsers){
+    if(err){
+      console.log(err);
+    }else{
+      if(foundUsers){
+        res.render("secrets", {usersWithSecrets: foundUsers});
+      }
+    }
+  });
+});
+
 const userSchema = new mongoose.Schema({
   email: String,
-  password: String
+  password: String,
+  secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
